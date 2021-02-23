10ece608bcae8098906281d58a9029cec88af353
BeatenByBacon <35689334+BeatenByBacon@users.noreply.github.com>
2021-02-23 22:57:24 +0100

    Added new Items
    Added EnemySpawner script
    Added EnemyMovement script
    Added Waypoints script
    
    Added 2 Ground-Tiles sprites
    Added 1 Tree
    
    Added Timer so it displays when the next wave is coming

---
+++ TowerDefense-Projekt/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public float speed;
+    private Waypoints wPoints;
+    private int waypointIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //enemy find waypoints script in scene
+        wPoints = GameObject.FindGameObjectWithTag("Waypoints").GetComponent<Waypoints>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Moves the enemy towards the next waypoint
+        transform.position = Vector2.MoveTowards(transform.position, wPoints.waypoints[waypointIndex].position, speed * Time.deltaTime);
+
+        //if the distance between enemy and waypoint is < 0.1f; increase waypoint index (so enemy focuses next waypoint)
+        if(Vector2.Distance(transform.position, wPoints.waypoints[waypointIndex].position) < 0.1f)
+        {
+            waypointIndex++;
+        }
+        if(waypointIndex == wPoints.waypoints.Length)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
